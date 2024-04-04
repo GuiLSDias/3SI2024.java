642b6d64326538eb1e43f19361a4ec54f4ac7263
GuiLSDias <glsd273@gmail.com>
2024-04-04 11:17:56 -0300

    Commit

---
+++ src/Interfaces/Quadrado.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class Quadrado implements AreaCalculavel{
+    private int lado;
+    public Quadrado(int lado) {
+        this.lado = lado;
+    }
+    public double calculaArea() {
+        return this.lado * this.lado;
+    }
+}
